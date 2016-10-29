01846115e6dc4b9628c19942b3b227b7c1e006e0
Evan <evan@bjjmapper.com>
2016-10-29 17:12:49 +0300

    Queue worker uses .env

--- locations_queue_worker.rb
+++ locations_queue_worker.rb
@@ -9,9 +9,9 @@ include Mongo
 WAIT_INTERVAL = 10.0
 RESQUE_LOCATIONS_QUEUE = "locations"
 
-Resque.mongo = MongoClient.new(DATABASE_HOST , DATABASE_PORT).db(DATABASE_QUEUE_DB)
+Resque.mongo = MongoClient.new(LocationFetchService::DATABASE_HOST , LocationFetchService::DATABASE_PORT).db(LocationFetchService::DATABASE_QUEUE_DB)
 
-STDOUT.puts "Starting locations queue worker on #{DATABASE_HOST}:#{DATABASE_PORT}/#{DATABASE_QUEUE_DB}"
+STDOUT.puts "Starting locations queue worker on #{LocationFetchService::DATABASE_HOST}:#{LocationFetchService::DATABASE_PORT}/#{LocationFetchService::DATABASE_QUEUE_DB}"
 
 worker = Resque::Worker.new(RESQUE_LOCATIONS_QUEUE)
 worker.verbose = worker.very_verbose = true
