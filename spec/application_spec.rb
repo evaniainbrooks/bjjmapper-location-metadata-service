59064ed09da2ded6a0619e696e35359925803eea
Evan <evan@bjjmapper.com>
2016-11-17 20:36:15 +0200

    Fix failing specs

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -9,23 +9,25 @@ describe 'LocationFetchService' do
     end
   end
   describe 'POST /search/async' do
+    let(:content_type) { { 'CONTENT_TYPE' => 'application/json' } }
     let(:common_params) { { api_key: LocationFetchService::APP_API_KEY } }
+    let(:api_key) { LocationFetchService::APP_API_KEY }
     context 'without location parameter' do
       let(:request) { {} }
       it 'returns 400 bad request' do
-        post '/search/async', common_params.merge(request)
+        post "/search/async?api_key=#{api_key}", request
         last_response.status.should eq 400
       end
     end
     context 'with location.id parameter' do
-      let(:request) { { location: { id: 123, lat: 80.0, lng: 80.0 }  } }
+      let(:request) { { location: { id: 123, lat: 80.0, lng: 80.0 }  }.to_json }
       context 'without scope parameter' do
         before do
           Resque.should_receive(:enqueue).with(GooglePlacesSearchJob, anything)
           Resque.should_receive(:enqueue).with(YelpSearchJob, anything)
         end
         it 'enqueues search jobs and returns 202' do
-          post '/search/async', common_params.merge(request)
+          post "/search/async?api_key=#{api_key}", request, content_type
           last_response.status.should eq 202
         end
       end
@@ -34,18 +36,18 @@ describe 'LocationFetchService' do
           Resque.should_receive(:enqueue).with(GooglePlacesSearchJob, anything)
         end
         it 'enqueues search job for the scope and returns 202' do
-          post '/search/async', common_params.merge(request).merge(scope: 'google')
+          post "/search/async?api_key=#{api_key}&scope=google", request, content_type
           last_response.status.should eq 202
         end
       end
     end
     context 'without location.id parameter' do
-      let(:request) { { location: { lat: 80.0, lng: 80.0 }  } }
+      let(:request) { { location: { lat: 80.0, lng: 80.0 }  }.to_json }
       before do
         Resque.should_receive(:enqueue).with(IdentifyCandidateLocationsJob, anything)
       end
       it 'enqueues an IdentifyCandidateLocationsJob and returns 202' do
-        post '/search/async', common_params.merge(request)
+        post "/search/async?api_key=#{api_key}", request, content_type
         last_response.status.should eq 202
       end
     end
