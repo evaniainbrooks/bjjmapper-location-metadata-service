a423c4746cd2fe2f1c583d07389a211c0d3038a6
Evan <evan@bjjmapper.com>
2017-02-14 15:34:25 +0300

    Script to create indexes

---
+++ script/create_indexes.rb
@@ -0,0 +1,16 @@
+require 'mongo'
+require_relative '../config'
+
+client = Mongo::Client.new("mongodb://#{LocationFetchService::DATABASE_HOST}:#{LocationFetchService::DATABASE_PORT}/#{LocationFetchService::DATABASE_APP_DB}")
+
+client[:facebook_pages].indexes.create_one(bjjmapper_location_id: 1, facebook_id: 1)
+client[:facebook_photos].indexes.create_one(width: 1, height: 1, album_id: 1, facebook_id: 1, photo_id: 1)
+
+client[:yelp_businesses].indexes.create_one(bjjmapper_location_id: 1, yelp_id: 1)
+client[:yelp_photos].indexes.create_one(bjjmapper_location_id: 1, yelp_id: 1, url: 1)
+client[:yelp_reviews].indexes.create_one(yelp_id: 1, time_created: 1, user_name: 1)
+
+client[:google_places_spots].indexes.create_one(bjjmapper_location_id: 1, place_id: 1)
+client[:google_places_photos].indexes.create_one(place_id: 1, photo_reference: 1)
+client[:google_places_reviews].indexes.create_one(place_id: 1, author_name: 1, time: 1)
+
